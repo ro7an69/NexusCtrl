22cf4e67cef0fdec2fdbbedbb416a76176c8b2b4
divrajsahney <65025124+divrajsahney@users.noreply.github.com>
2023-12-09 17:42:51 +0400

    Remove submodule: CSIT-321-PROJECT-NEXUS-CTRL-

--- CSIT-321-PROJECT-NEXUS-CTRL-/frontend/src/pages/firebaseSaveUtils.js
+++
@@ -1,8 +0,0 @@
-import { database } from './sign';
-import { ref, set } from 'firebase/database';
-
-export const saveSettingsToFirebase = (settingsData) => {
-
-    const settingsRef = ref(database, settingsData.uid); // Use the UID in the path
-    return set(settingsRef, settingsData);
-  };
